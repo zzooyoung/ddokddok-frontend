80d6ac6b8c965332466cf2ee97a53267c6dbb260
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-06-23 20:39:12 +0900

    스터디 상세페이지 수정
    스터디 상세페이지
    - 홈, 멤버, 기능삽입

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Home from './Home/home_main';
 import Login from './Login/login_main';
 import Register from './Register/register_main';
 
+<<<<<<< Updated upstream
 import Studylist from './Studylist/studylist_main';
 import QnA from './QnA/qna_main';
 import Mypage from './Mypage/mypage_main';
 import CreateStudy from './CreateStudy/create_study';
 import './App.css';
+=======
 
 function App() {
   return (
@@ -27,7 +29,13 @@ function App() {
               <Route path="/studylist" element={<Studylist />} />
               <Route path="/qna" element={<QnA />} />
               <Route path="/mypage" element={<Mypage />} />
+<<<<<<< Updated upstream
               <Route path="/createstudy" element={<CreateStudy/>} />
+=======
+              <Route path="/createstudy" element={<CreateStudy />} />
+              <Route path="/study" element={<Study/>} />
+              <Route path="/StudyDetails/Home/Cheif" element={<CheifStudyHome />}/>
+>>>>>>> Stashed changes
             </Routes>
             </div>
         </div>
