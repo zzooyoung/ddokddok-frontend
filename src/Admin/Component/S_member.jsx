b9314f4c3f4350cc2cbed3ce585a8536f1d2c257
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-06-24 08:39:44 +0900

    스터디생성,관리자페이지
    스터디생성,관리자페이지

--- src/Admin/Component/S_member.jsx
+++ src/Admin/Component/S_member.jsx
@@ -37,7 +37,7 @@ function S_member() {
 
     fetchMemberData();
   }, []);
-
+  console.log(sessionStorage.getItem("id"));
 
   const handleAccept = async (memberId) => {
     const memberToAccept = members.find(member => member.member_id === memberId);
@@ -45,9 +45,15 @@ function S_member() {
       try {
         // Send a POST request to the server to update the member status
         await axios.post(`http://192.168.0.98:8080/study/member/accept`, {
+<<<<<<< Updated upstream
           member_id: sessionStorage.getItem("id"),
           memberId: memberId, // Make sure the field names match what your API expects
           studyId: memberToAccept.study_id, // Assuming study_id is available in the member object
+=======
+          member_id: members.member_id,
+          memberId: sessionStorage.getItem("id"), 
+          studyId: memberToAccept.study_id, 
+>>>>>>> Stashed changes
         });
   
         // Update local state to reflect the new status of the member
